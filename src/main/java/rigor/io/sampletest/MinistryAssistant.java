c952703e92b174188d46b9e0ed53a9b7259e60c3
unknown <rigorei.sarmiento@gmail.com>
2023-06-25 12:59:28 +0800

    Initial commit

---
+++ src/main/java/rigor/io/sampletest/MinistryAssistant.java
@@ -0,0 +1,13 @@
+package rigor.io.sampletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinistryAssistant {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinistryAssistant.class, args);
+	}
+
+}
---
+++ src/test/java/rigor/io/sampletest/SampleTestApplicationTests.java
@@ -0,0 +1,13 @@
+package rigor.io.sampletest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
